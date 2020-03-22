fb4286f4a27ceef50d93d67b8ed1cfd0e4f677f5
emeryck <pokspid@gmail.com>
2020-03-22 18:16:07 +0100

    fix: bug we didn t display av[0]

--- src/display.c
+++ src/display.c
@@ -47,7 +47,7 @@ void display_key(keyt_t *key)
 int set_up_key(keyt_t *key, int ac, char *av[])
 {
     if (ac == 2 && !my_strcmp(av[1], "--help"))
-        return (display_usage(av[1]));
+        return (display_usage(av[0]));
     key->key_left = str_copy("ˆEOC");
     key->key_right = str_copy("ˆEOD");
     key->key_turn = str_copy("ˆEOA");
